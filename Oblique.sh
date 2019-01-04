a8560f6a57bef5d866c5daa83b015e4595e73d40
Ciaran Robb <ciaranrobb@ighpphh0065.ighpp.aber.ac.uk>
2019-01-04 15:05:31 +0000

    tapioca and campari changes

--- Oblique.sh
+++ Oblique.sh
@@ -10,15 +10,17 @@ EXTENSION=JPG
 Algorithm=Statue
 wait_for_mask=true
 ZOOM=2
+dist=5
 
-while getopts "e:a:smz:h" opt; do
+while getopts "e:a:sdmz:h" opt; do
   case $opt in
     h)
       echo "Run workflow for point cloud from culture 3d algo."
-      echo "usage: Oblique.sh -e JPG -a Statue -z 1"
+      echo "usage: Oblique.sh -e JPG -a Statue -d 10 -z 1"
       echo "	-e EXTENSION   : image file type (JPG, jpg, TIF, png..., default=JPG)."
       echo "	-a Algorithm   : type of algo eg BigMac, MicMac, Forest, Statue etc."
       echo "	-s             : Do not use 'Schnaps' optimised homologous points (does by default)."
+      echo "	-d             : distance between photos for image pairs"
       echo "	-m             : Pause for Mask before correlation (does not by default)."
       echo "	-z ZOOM        : Zoom Level (default=2)"
       echo "	-h	  : displays this message and exits."
@@ -35,7 +37,10 @@ while getopts "e:a:smz:h" opt; do
       ZOOM=$OPTARG
       ;;
 	s)
-      use_Schnaps=$OPTARG
+      use_Schnaps=true
+      ;; 
+	d)
+      dist=$OPTARG
       ;; 
 	m)
       wait_for_mask=true
@@ -56,7 +61,7 @@ echo "using exif data"
 mm3d XifGps2Txt .*$EXTENSION
 #Get the GNSS data out of the images and convert it to a xml orientation folder (Ori-RAWGNSS), also create a good RTL (Local Radial Tangential) system.
 mm3d XifGps2Xml .*$EXTENSION RAWGNSS_N
-mm3d OriConvert "#F=N X Y Z" GpsCoordinatesFromExif.txt RAWGNSS_N ChSys=DegreeWGS84@RTLFromExif.xml MTD1=1 NameCple=FileImagesNeighbour.xml CalcV=1
+mm3d OriConvert "#F=N X Y Z" GpsCoordinatesFromExif.txt RAWGNSS_N ChSys=DegreeWGS84@RTLFromExif.xml MTD1=1 NameCple=FileImagesNeighbour.xml CalcV=1 DN=$dist
 
 #Use the GpsCoordinatesFromExif.txt file to create a xml orientation folder (Ori-RAWGNSS_N), and a file (FileImagesNeighbour.xml) detailing what image sees what other image (if camera is <50m away with option DN=50)
 
@@ -70,22 +75,24 @@ mm3d OriConvert "#F=N X Y Z" GpsCoordinatesFromExif.txt RAWGNSS_N ChSys=DegreeWG
 mogrify -resize 2000 *.JPG 
 #    mm3d Tapioca File FileImagesNeighbour.xml -1 @SFS
 #fi 
+#File FileImagesNeighbour.xml
 
-mm3d Tapioca File FileImagesNeighbour.xml -1 @SFS
+# If the camera positions are all over the shop its better to use the ALL option
+mm3d Tapioca All .*$EXTENSION  -1 @SFS
 
 
-if [ "use_Schnaps" = true ]; then
-    mm3d Schnaps .*$EXTENSION MoveBadImgs=1
+#if [ "use_Schnaps" = true ]; then
+mm3d Schnaps .*$EXTENSION MoveBadImgs=1 
 
-    #Compute Relative orientation (Arbitrary system)
-    mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary SH=_mini
-else
-    mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary
-fi
+    #Compute Relative orientation (Arbitrary system) 
+mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary SH=_mini
+#else
+ #   mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary
+#fi
 
 # This lot screws it up when not all nadir!!!! 
 #Transform to  RTL system
-#mm3d CenterBascule .*$EXTENSION Arbitrary RAWGNSS_N Ground_Init_RTL
+mm3d CenterBascule .*$EXTENSION Arbitrary RAWGNSS_N Ground_Init_RTL
 
 #Change system to final cartographic system  
 #if [ $CSV != none ]; then 
@@ -96,23 +103,24 @@ fi
 
 #else
 
-#mm3d Campari .*$EXTENSION Ground_Init_RTL Ground_RTL EmGPS=[RAWGNSS_N,1] AllFree=1 SH=_mini
+mm3d Campari .*$EXTENSION Ground_Init_RTL Ground_RTL EmGPS=[RAWGNSS_N,1] AllFree=1 SH=_mini 
 
 # This lot here assumes a geographic coordinate system which we do not have with SaisieMasq output
 
 #mm3d ChgSysCo  .*$EXTENSION Ground_RTL RTLFromExif.xml@SysUTM.xml Ground_UTM
 #mm3d OriExport Ori-Ground_UTM/.*xml CameraPositionsUTM.txt AddF=1
+mm3d AperiCloud .*$EXTENSION Ground_RTL SH=_mini Out=withcams.ply
 
-mm3d AperiCloud .*$EXTENSION Arbitrary SH=_mini  WithCam=0
+mm3d AperiCloud .*$EXTENSION Ground_RTL  WithCam=0 SH=_mini
 
   
 #HERE, MASKING COULD BE DONE!!!
 if [ "$wait_for_mask" = true ]; then
-    mm3d SaisieMasqQT *.ply
+    mm3d SaisieMasqQT AperiCloud_Ground_RTL__mini.ply
 	read -rsp $'Press any key to continue...\n' -n1 key
 fi 
 	
 #Do the correlation of the images
 
-mm3d C3DC $Algorithm .*$EXTENSION Arbitrary ZoomF=$ZOOM Masq3D=AperiCloud_Arbitrary__mini_polyg3d.xml
+mm3d C3DC $Algorithm .*$EXTENSION Arbitrary ZoomF=$ZOOM Masq3D=AperiCloud_Ground_RTL__mini_polyg3d.xml
 
