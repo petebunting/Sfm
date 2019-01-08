d6a5172f64798ad82501ad54f24f965798cb9435
Ciaran1981 <ciaran.robb@gmail.com>
2019-01-08 11:16:51 +0000

    updates to ob and pims

--- Oblique.sh
+++ Oblique.sh
@@ -1,7 +1,7 @@
 # This is for oblique imagery - a work in progress
 
 # Author Ciaran Robb
-# Little changed form L.Girod - credit to him!
+# Based on the L.Girod original - credit to him!
 # Aberystwyth University
 
 
@@ -12,15 +12,18 @@ wait_for_mask=true
 ZOOM=2
 dist=5
 match=none
+CSV=0
 
-while getopts "e:a:m:s:d:msk:z:h" opt; do
+while getopts "e:a:m:csv:u:s:d:msk:z:h" opt; do
   case $opt in
     h)
       echo "Run workflow for point cloud from culture 3d algo."
-      echo "usage: Oblique.sh -e JPG -a Statue -d 10 -z 1"
+      echo "usage: Oblique.sh -e JPG -a Statue -d 10 -u "30 +north" -z 1"
       echo "	-e EXTENSION   : image file type (JPG, jpg, TIF, png..., default=JPG)."
       echo "	-a Algorithm   : type of algo eg BigMac, MicMac, Forest, Statue etc."
       echo "	-m match         : exaustive matching" 
+      echo "	-csv CSV         : Whether to use a csv file."
+      echo "	-u UTM         : UTM zone."
       echo "	-s             : Do not use 'Schnaps' optimised homologous points (does by default)."
       echo "	-d             : distance between photos for image pairs"
       echo "	-msk             : Pause for Mask before correlation (does not by default)."
@@ -32,12 +35,18 @@ while getopts "e:a:m:s:d:msk:z:h" opt; do
 	e)
       EXTENSION=$OPTARG
       ;;
-  algo)
+    a)
       Algorithm=$OPTARG
       ;; 
     m)
       match=$OPTARG 
       ;;     
+    csv)
+      match=$OPTARG 
+      ;;
+	u)
+      UTM=$OPTARG 
+      ;;   
 	z)
       ZOOM=$OPTARG
       ;;
@@ -61,13 +70,32 @@ while getopts "e:a:m:s:d:msk:z:h" opt; do
   esac
 done
 
+ 
+#create UTM file (after deleting any existing one) Only required for csv option but hey
+rm SysUTM.xml
+echo "<SystemeCoord>                                                                                              " >> SysUTM.xml
+echo "         <BSC>                                                                                              " >> SysUTM.xml
+echo "            <TypeCoord>  eTC_Proj4 </TypeCoord>                                                             " >> SysUTM.xml
+echo "            <AuxR>       1        </AuxR>                                                                   " >> SysUTM.xml
+echo "            <AuxR>       1        </AuxR>                                                                   " >> SysUTM.xml
+echo "            <AuxR>       1        </AuxR>                                                                   " >> SysUTM.xml
+echo "            <AuxStr>  +proj=utm +zone="$UTM "+ellps=WGS84 +datum=WGS84 +units=m +no_defs   </AuxStr>        " >> SysUTM.xml
+echo "                                                                                                            " >> SysUTM.xml
+echo "         </BSC>                                                                                             " >> SysUTM.xml
+echo "</SystemeCoord>                                                                                             " >> SysUTM.xml
 
-echo "using exif data"
-mm3d XifGps2Txt .*$EXTENSION
-#Get the GNSS data out of the images and convert it to a xml orientation folder (Ori-RAWGNSS), also create a good RTL (Local Radial Tangential) system.
-mm3d XifGps2Xml .*$EXTENSION RAWGNSS_N
-mm3d OriConvert "#F=N X Y Z" GpsCoordinatesFromExif.txt RAWGNSS_N ChSys=DegreeWGS84@RTLFromExif.xml MTD1=1 NameCple=FileImagesNeighbour.xml CalcV=1 DN=$dist
-
+if [  "$CSV"=1  ]; then 
+    echo "using csv file"
+    cs=*.csv
+    mm3d OriConvert OriTxtInFile $cs RAWGNSS_N ChSys=DegreeWGS84@SysUTM.xml MTD1=1 NameCple=FileImagesNeighbour.xml CalcV=1   
+    sysCort_make.py -csv $cs 
+else
+    echo "using exif data"
+    mm3d XifGps2Txt .*$EXTENSION
+    #Get the GNSS data out of the images and convert it to a xml orientation folder (Ori-RAWGNSS), also create a good RTL (Local Radial Tangential) system.
+    mm3d XifGps2Xml .*$EXTENSION RAWGNSS_N
+    mm3d OriConvert "#F=N X Y Z" GpsCoordinatesFromExif.txt RAWGNSS_N ChSys=DegreeWGS84@RTLFromExif.xml MTD1=1 NameCple=FileImagesNeighbour.xml CalcV=1 DN=$dist
+fi
 #Use the GpsCoordinatesFromExif.txt file to create a xml orientation folder (Ori-RAWGNSS_N), and a file (FileImagesNeighbour.xml) detailing what image sees what other image (if camera is <50m away with option DN=50)
 
 
@@ -91,34 +119,18 @@ else
 fi
 
 
-#if [ "use_Schnaps" = true ]; then
 mm3d Schnaps .*$EXTENSION MoveBadImgs=1 
 
-    #Compute Relative orientation (Arbitrary system) 
-mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary SH=_mini
-#else
- #   mm3d Tapas Fraser .*$EXTENSION Out=Arbitrary
-#fi
 
 # This lot screws it up when not all nadir!!!! 
 #Transform to  RTL system
 mm3d CenterBascule .*$EXTENSION Arbitrary RAWGNSS_N Ground_Init_RTL
 
 #Change system to final cartographic system  
-#if [ $CSV != none ]; then 
-   # mm3d Campari .*$EXTENSION Ground_Init_RTL Ground_UTM EmGPS=[RAWGNSS,1] AllFree=1 SH=_mini
-    # For reasons unknown this screws it up from csv
-    #mm3d ChgSysCo  .*$EXTENSION Ground_RTL SysCoRTL.xml@SysUTM.xml Ground_UTM
-#    mm3d AperiCloud .*$EXTENSION Ori-Ground_RTL SH=_mini
 
-#else
-
+ 
 mm3d Campari .*$EXTENSION Ground_Init_RTL Ground_RTL EmGPS=[RAWGNSS_N,1] AllFree=1 SH=_mini 
 
-# This lot here assumes a geographic coordinate system which we do not have with SaisieMasq output
-
-#mm3d ChgSysCo  .*$EXTENSION Ground_RTL RTLFromExif.xml@SysUTM.xml Ground_UTM
-#mm3d OriExport Ori-Ground_UTM/.*xml CameraPositionsUTM.txt AddF=1
 mm3d AperiCloud .*$EXTENSION Ground_RTL SH=_mini Out=withcams.ply
 
 mm3d AperiCloud .*$EXTENSION Ground_RTL  WithCam=0 SH=_mini
@@ -133,4 +145,3 @@ fi
 #Do the correlation of the images
 
 mm3d C3DC $Algorithm .*$EXTENSION Arbitrary ZoomF=$ZOOM Masq3D=AperiCloud_Ground_RTL__mini_polyg3d.xml
-
